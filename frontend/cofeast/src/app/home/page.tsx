c2690376b4fc2e398520f07054ba36184a9a2b5f
FangyuKung <fish19921026@gmail.com>
2025-11-08 21:39:21 +0800

    feat: create home page ui and add card components

---
+++ frontend/cofeast/src/app/home/page.tsx
@@ -0,0 +1,72 @@
+/*
+ * @Author: Fangyu Kung
+ * @Date: 2025-11-08 16:20:13
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-11-08 21:31:35
+ * @FilePath: /frontend/cofeast/src/app/home/page.tsx
+ */
+"use client";
+import { Wrapper } from "@/components/layout/layoutStyle";
+import theme from "@/theme";
+import { Box, CardMedia, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import ActivityCard from "./components/ActivityCard";
+import HorizontalCard from "./components/horizontalCard";
+
+/*
+ * @Author: Fangyu Kung
+ * @Date: 2025-11-08 16:20:13
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-11-08 20:54:35
+ * @FilePath: /frontend/cofeast/src/app/home/page.tsx
+ */
+const HomePage = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <CardMedia
+          component="img"
+          image="/images/banner.jpg"
+          sx={{
+            width: "100%",
+            margin: "16px 0px",
+            borderRadius: "12px",
+          }}
+        />
+        <Typography variant="h2" color="text.primary" mt={6}>
+          共饗食光 CoFeast
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1.5,
+            marginTop: 3,
+            marginBottom: 5,
+            flexWrap: "wrap",
+          }}
+        >
+          <ActivityCard
+            link="#"
+            title="我要發起共餐"
+            imageUrl="/images/temp.jpg"
+          />
+          <ActivityCard
+            link="#"
+            title="我要響應共餐"
+            imageUrl="/images/temp.jpg"
+          />
+        </Box>
+        <Typography variant="h2" color="text.primary" my={3}>
+          愛心餐食補給站
+        </Typography>
+        <HorizontalCard
+          link="#"
+          title="查看愛心據點"
+          imageUrl="/images/temp2.jpg"
+        />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
+
+export default HomePage;
--- frontend/cofeast/src/app/layout.tsx
+++ frontend/cofeast/src/app/layout.tsx
@@ -2,11 +2,12 @@
  * @Author: Fangyu Kung
  * @Date: 2025-11-08 15:16:23
  * @LastEditors: Do not edit
- * @LastEditTime: 2025-11-08 15:47:48
+ * @LastEditTime: 2025-11-08 17:38:46
  * @FilePath: /frontend/cofeast/src/app/layout.tsx
  */
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 // or `v1X-appRouter` if you are using Next.js v1X
+import "@/app/globals.css";
 import theme from "@/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { Roboto } from "next/font/google";
