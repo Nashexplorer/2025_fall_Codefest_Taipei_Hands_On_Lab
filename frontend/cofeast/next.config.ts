dbf5b941f65dada747a6b0905ff3178cb391ff39
FangyuKung <fish19921026@gmail.com>
2025-11-08 21:38:14 +0800

    chore: add mui related plugin

--- frontend/cofeast/next.config.ts
+++ frontend/cofeast/next.config.ts
@@ -1,7 +1,21 @@
+/*
+ * @Author: Fangyu Kung
+ * @Date: 2025-11-08 15:16:23
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-11-08 20:29:53
+ * @FilePath: /frontend/cofeast/next.config.ts
+ */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://gongcan-api-d4vsmusihq-de.a.run.app/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
