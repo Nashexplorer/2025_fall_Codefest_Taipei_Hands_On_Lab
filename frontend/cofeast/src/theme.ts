26457f7adab850c4d0e082de3b3a5c1c3503622e
FangyuKung <fish19921026@gmail.com>
2025-11-08 15:49:51 +0800

    feat: added Mui plugin and modified the initial layout

---
+++ frontend/cofeast/src/theme.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Fangyu Kung
+ * @Date: 2025-11-08 15:44:24
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-11-08 15:44:31
+ * @FilePath: /frontend/cofeast/src/theme.ts
+ */
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "var(--font-roboto)",
+  },
+});
+
+export default theme;
